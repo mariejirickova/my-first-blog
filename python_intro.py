0c4b720aa1619b50b0355c0b3b7c57a04a8f377b
Admin <mariejirickova@seznam.cz>
2015-05-16 18:05:49 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,9 @@
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+a = len(girls)
+
+print ('Recieved ' + str(a) + ' names.')
+def hi(name):
+    print('Hi ' + name + '!')
+for name in girls:
+    hi(name)
+print('Have a nice day')
